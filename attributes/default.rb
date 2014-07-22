da761b910d12768a7b1ebc79bb43816d4ededfd7
Andrew Myhre <andrew.myhre@gmail.com>
2014-07-22 17:19:58 -0400

    sets exe_path node attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,4 +7,17 @@ if node['platform_family'] == 'windows'
   end
   default['chocolatey']['bin_path'] = ::File.join(node['chocolatey']['path'], 'bin')
   default['chocolatey']['upgrade']  = true
+
+  if (::File.exist?(::File.join(node['chocolatey']['bin_path'], 'chocolatey.exe')))
+    default['chocolatey']['chocolatey_exe'] = 'chocolatey.exe'
+  elsif (::File.exist?(::File.join(node['chocolatey']['bin_path'], 'choco.exe')))
+    default['chocolatey']['chocolatey_exe'] = 'choco.exe'
+  elsif (::File.exist?(::File.join(node['chocolatey']['bin_path'], 'chocolatey.bat')))
+    default['chocolatey']['chocolatey_exe'] = 'chocolatey.bat'
+  elsif (::File.exist?(::File.join(node['chocolatey']['bin_path'], 'choco.bat')))
+    default['chocolatey']['chocolatey_exe'] = 'choco.bat'
+  end
+
+  default['chocolatey']['exe_path'] = ::File.join(node['chocolatey']['bin_path'], node['chocolatey']['chocolatey_exe'])
+
 end
