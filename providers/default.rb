e9a339ac2336ca76bebfc862dc9da9548ac94078
Owain Perry <owain.perry@justgiving.com>
2016-09-05 14:17:02 +0100

    using quotes for arguments quotes

--- providers/default.rb
+++ providers/default.rb
@@ -268,7 +268,7 @@ def jg_cmd_args
   output = ''
   output += " --allow-empty-checksums " # not ideal but should move this upto resource level
   output += " --source #{@current_resource.source}" if @current_resource.source
-  output += " --ia '#{@current_resource.args}'" unless @current_resource.args.to_s.empty?
+  output += " --ia \"#{@current_resource.args}\"" unless @current_resource.args.to_s.empty?
   @current_resource.options.each do |k, v|
     output += " --#{k}"
     output += " #{v}" if v
